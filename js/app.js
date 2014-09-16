e27fae5f515f6622a0eac1fb1ed65d8a8b27d844
Ranjit Jena <rjena0406@gmail.com>
2014-09-16 12:01:07 +0530

    3.0.3

--- js/app.js
+++ js/app.js
@@ -2,16 +2,22 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    var service = new EmployeeService();
+    /*var service = new EmployeeService();
     service.initialize().done(function () {
         console.log("Service initialized");
+    });*/
+
+    var service = new EmployeeService();
+    service.initialize().done(function () {
+      renderHomeView();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+    //$('.search-key').on('keyup', findByName);
+    
+    //$('.help-btn').on('click', function() {
+     //   alert("Employee Directory v3.4");
+    //});
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
@@ -25,6 +31,15 @@
             }
         });
     }
+
+    function renderHomeView() {
+      var html =
+        "<h1>Directory</h1>" +
+        "<input class='search-key' type='search' placeholder='Enter name'/>" +
+        "<ul class='employee-list'></ul>";
+      $('body').html(html);
+      $('.search-key').on('keyup', findByName);
+  }
 
 }());
 document.addEventListener('deviceready', function () {
