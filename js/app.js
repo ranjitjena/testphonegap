e9bf35c7d3dca3e7e5004f61d224c3f7888c9be7
Ranjit Jena <rjena0406@gmail.com>
2014-09-16 09:14:54 +0530

    2.0.5

--- js/app.js
+++ js/app.js
@@ -28,15 +28,3 @@
 
 }());
 
-document.addEventListener('deviceready', function () {
-  if (navigator.notification) { // Override default HTML alert with native dialog
-      window.alert = function (message) {
-          navigator.notification.alert(
-              message,    // message
-              null,       // callback
-              "Workshop", // title
-              'OK'        // buttonName
-          );
-      };
-  }
-}, false);
