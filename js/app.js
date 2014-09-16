5bf385c331c807b8e1393b3b3e5001eb1013f2d3
Ranjit Jena <rjena0406@gmail.com>
2014-09-16 13:53:45 +0530

    3.0.7
    Hardware Acceleration

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@
   EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
   EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
+  var slider = new PageSlider($('body'));
+
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     /*var service = new EmployeeService();
@@ -19,12 +21,14 @@
     var service = new EmployeeService();
     service.initialize().done(function () {
         router.addRoute('', function() {
-            $('body').html(new HomeView(service).render().$el);
+           // $('body').html(new HomeView(service).render().$el);
+           slider.slidePage(new HomeView(service).render().$el);
         });
 
         router.addRoute('employees/:id', function(id) {
             service.findById(parseInt(id)).done(function(employee) {
-                $('body').html(new EmployeeView(employee).render().$el);
+                //$('body').html(new EmployeeView(employee).render().$el);
+                slider.slidePage(new EmployeeView(employee).render().$el);
             });
         });
 
